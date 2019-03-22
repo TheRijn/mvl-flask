52a3d5cfc491db85a5c8572ec6e83efd15b3c587
Marijn Jansen <marijn@jansen.onl>
2019-03-22 13:28:02 +0100

    fixed

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,13 +1,16 @@
 $('#detailModal').on('show.bs.modal', function (event) {
     let card = $(event.relatedTarget); // Button that triggered the modal
     let post_id = card.data('post_id');
-
+    $('#details-intro').html("<div class=\"spinner-border\" role=\"status\">\n" +
+        "  <span class=\"sr-only\">Loading...</span>\n" +
+        "</div>");
+    $('#details-description').html('');
     $.post(`/api/post/${post_id}`, data => {
 
         let post = data.post;
         console.log(post);
 
-        $('#details-title').text(post.title);
+        // $('#details-title').text(post.title);
         $('#details-intro').html(post.intro);
         $('#details-description').html(post.description);
 
